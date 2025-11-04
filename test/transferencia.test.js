3b9734a63ada530f66de574f3a596908a71ced87
cremope <novoa.lopez@gmail.com>
2025-11-04 11:14:57 -0300

    Adiciona beforeEach

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,8 +5,13 @@ const { obterToken } = require('../helpers/autenticacao');
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
+        let token;
+        
+        beforeEach(async () => {
+            token = await obterToken('julio.lima', '123456');
+        })
+
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async () => {
-            const token = await obterToken('julio.lima', '123456');
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -26,7 +31,6 @@ describe('Transferencias', () => {
         });
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de 10 reais', async () => {
-            const token = await obterToken('julio.lima', '123456');
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
